7c842995933a535f8c5c2b51cdf6c2d09de30a85
Martin Lupa <martinlupa@Martins-MacBook-Pro.local>
2025-08-01 13:46:50 +1000

    chore: cleanup, bricks is WIP

--- mercadopago-bricks/app/mocks/customer.ts
+++
@@ -1,9 +0,0 @@
-export const customer = {
-    email: "martin@gmail.com",
-    firstName: "Martin",
-    lastName: "Lupa",
-    address: "123 Main St",
-    city: "Cityville",
-    zipCode: "3000",
-    phone: "111 111 1111",
-}
